883a8b24b80c4a3d5cc6a45b3624188c8675943a
Rehan <rehan7ctech@gmail.com>
2025-05-07 20:46:58 +0500

    hero img add on mbl

--- src/containers/HeroContainer/Hero.tsx
+++ src/containers/HeroContainer/Hero.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import styles from "./hero.module.scss"
 import { MdArrowOutward } from "react-icons/md";
 import Form from '../Form/Form';
+import dynamic from 'next/dynamic';
+const MediaQuery = dynamic(()=> import('react-responsive'), { ssr: false });
+import mblHero from "@/assets/mblHero.png"
+import Image from 'next/image';
 
 type Props = {}
 
@@ -21,7 +25,17 @@ const Hero = (props: Props) => {
               <source src="https://videos.ctfassets.net/mi665aivb0at/xRSpukKuiLPVNxfEnAsTp/a6a1c03c9cb07431e91ec93e10886b63/Cubix_Home_Hero_Video.webm" />
               Your browser does not support the video tag.
           </video> */}
-          <div className={styles.imgBackground}><img src="https://images.unsplash.com/photo-1678203699263-917199c725b2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGVybyUyMGltYWdlfGVufDB8fDB8fHww" alt="" /></div>
+
+          <MediaQuery minWidth={550}>
+            <div className={styles.imgBackground}>
+              <img src="https://images.unsplash.com/photo-1678203699263-917199c725b2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGVybyUyMGltYWdlfGVufDB8fDB8fHww" alt="" />
+            </div>
+          </MediaQuery>
+          <MediaQuery maxWidth={549}>
+            <div className={styles.imgBackground}>
+              <Image src={mblHero} alt="" height={1000} width={1000} />
+            </div>
+          </MediaQuery>
           <div className={styles.content}>
             <div>
               <h1>We are <span>Experts </span> <br /> in <span> Software <br /> </span> <span>Creation.</span></h1>
